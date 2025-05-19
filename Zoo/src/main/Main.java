5ac751aab98392286a5bdfae59a19cc15b99de35
veraleon01 <miguelveraleon1003@gmail.com>
2025-05-18 21:54:08 -0500

    implementacion de la clase zoologico en el main

--- Zoo/src/main/Main.java
+++ Zoo/src/main/Main.java
@@ -1,11 +1,13 @@
 package main;
 
 
-public class Main {
+import view.ZoologicoVista;
+import controller.ZoologicoController;
 
-   
+public class Main {
     public static void main(String[] args) {
-       
+        ZoologicoVista vista = new ZoologicoVista();
+        new ZoologicoController(vista);
     }
-    
 }
+
